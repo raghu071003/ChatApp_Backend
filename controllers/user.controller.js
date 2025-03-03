a7c46292a1c549ce2be4cfaedcff1da25f2ed9ce
Sai Raghu Vamshi Koneti <131893227+raghu071003@users.noreply.github.com>
2025-03-04 00:09:10 +0530

    Bug Fixes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -234,7 +234,7 @@ import {GoogleGenerativeAI } from "@google/generative-ai"
 import dotenv from "dotenv"
 dotenv.config()
 const generateMessage = async(req,res)=>{
-    const genAI = new GoogleGenerativeAI('AIzaSyCX1jO_XaUfjURm7SNFFuSxVfiTqPu--A0');
+    const genAI = new GoogleGenerativeAI('');
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
     const prompt = "Explain how AI works";
     const result = await model.generateContent(prompt);
